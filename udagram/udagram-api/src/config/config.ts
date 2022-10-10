0ea1787e7ea990412c2a678f14ee968b69b2c870
mohamed952741 <mohamed952741@gmail.com>
2022-10-11 01:21:15 +0200

    new commit

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -10,7 +11,10 @@ export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
+  port: Number(process.env.PORT),
+  db_port: Number(process.env.DB_PORT),
   host: process.env.POSTGRES_HOST,
+  dialect: "postgres",
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
